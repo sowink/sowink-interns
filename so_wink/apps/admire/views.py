deb5d0836e9c87895c54e87382e9f830fec217cd
HaoQi Li <haoqili@mit.edu>
2011-07-24 15:19:26 -0700

    emails made better

--- so_wink/apps/admire/views.py
+++ so_wink/apps/admire/views.py
@@ -14,6 +14,8 @@ import json
 
 from django.contrib.auth.models import User
 
+from admire.models import Admire, ProfileVisit
+
 
 # urls.py's views. It renders the urls by putting in appropriate values into templates
 # each def 
@@ -34,26 +36,54 @@ def index(request):
     rendered = jingo.render(request, 'admire/index.html', ctx)
     return rendered
 
-def email(request, user_name):
-    print user_name
+def email(request, b_name):
+    admirer = ""
+    being_admired = ""
 
-    # get user
+    # get admirer
     try:
-        user = User.objects.get(username = user_name)
+        admirer = User.objects.get(username = request.user)
     except User.DoesNotExist:
-        return HttpResponse("Invalid username")
+        return HttpResponse("Invalid admirer username")
 
-    # email user
+    # get being_admired
     try:
-        user.email_user(subject="HaoQi Test Django Email", message="Haoqi :D:D:D:D")
-    except:
-        return HttpResponse("Failed to email")
+        being_admired = User.objects.get(username = b_name)
+    except User.DoesNotExist:
+        return HttpResponse("Invalid being_admired username")
 
-    print "hiiiiiiiii"
-    return HttpResponse("Your admire has been sent! <a href='/admire/guess/" + user_name + "'>That person will see</a>")
+
+    # email users 
+    # TODO: put messages in database?
+    sbj_to_b = "You got an Admirer! Guess who!"
+    msg_to_b = "You got an admirer. click here to find out who" # TODO: add url
+
+    sbj_to_a_success = "Your Admire was successfully sent. Click to track."
+    msg_to_a_success = "You can click on this link to see if " + b_name + " has guessed who you are. We will also email you when b starts to guess you."
+
+    sbj_to_a_fail = "There was a problem and your Admire was not sent. Please try again later."
+    msg_to_a_fail = "Sorry about that"
+
+    # 1. email being_admired
+    try:
+        being_admired.email_user(subject = sbj_to_b, message = msg_to_b)
+
+        # 2. email admirer -- success
+        try:
+            admirer.email_user(subject = sbj_to_a_success, message = msg_to_a_success)
+        except:
+            return HttpResponse("Email failed")
+    except:
+        # 2. email admirer -- fail
+        try:
+            admirer.email_user(subject = sbj_to_a_fail, message = msg_to_a_fail)
+        except:
+            return HttpResponse("Email failed")
+       
+        return HttpResponse("Email failed")
+
+    return HttpResponse("Your admire has been sent! <a href='/admire/guess/" + b_name + "'>That person will see</a>")
     
-#from django.views.decorators.csrf import csrf_protect
-#@csrf_protect
 def guess(request, user_name):
     print user_name
     if request.method == "POST":
